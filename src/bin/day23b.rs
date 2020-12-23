269546cf4f83e51ebfa3ed286221ae4102487f31
Diggory Blake <diggsey@googlemail.com>
2020-12-23 17:40:33 +0000

    Remove unused links

--- src/bin/day23b.rs
+++ src/bin/day23b.rs
@@ -6,8 +6,6 @@ const INPUT: &str = "792845136";
 struct Cup {
     value: u32,
     circle_next: usize,
-    circle_prev: usize,
-    num_next: usize,
     num_prev: usize,
 }
 
@@ -18,37 +16,27 @@ fn main() {
     let one_pos = early_values.iter().copied().position(|v| v == 1).unwrap();
 
     let mut values = Vec::new();
-    let mut prev_index = TOTAL_LEN - 1;
     for value in early_values.iter().copied() {
         let index = values.len();
         values.push(Cup {
             value,
-            circle_prev: prev_index,
             circle_next: index + 1,
             num_prev: early_values
                 .iter()
                 .copied()
                 .position(|v| v == value - 1)
                 .unwrap_or(TOTAL_LEN - 1),
-            num_next: early_values
-                .iter()
-                .copied()
-                .position(|v| v == value + 1)
-                .unwrap_or(early_values.len()),
         });
-        prev_index = index;
     }
     for value in 10..=TOTAL_LEN {
         values.push(Cup {
             value: value as u32,
-            circle_prev: value - 2,
             circle_next: value % TOTAL_LEN,
             num_prev: if value == 10 {
                 early_values.iter().copied().position(|v| v == 9).unwrap()
             } else {
                 value - 2
             },
-            num_next: if value == TOTAL_LEN { one_pos } else { value },
         })
     }
 
@@ -64,12 +52,9 @@ fn main() {
             dst = values[dst].num_prev;
         }
 
-        values[move_end].circle_prev = current;
         values[current].circle_next = move_end;
 
         let dst_end = mem::replace(&mut values[dst].circle_next, move1);
-        values[dst_end].circle_prev = move3;
-        values[move1].circle_prev = dst;
         values[move3].circle_next = dst_end;
 
         current = move_end;

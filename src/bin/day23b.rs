51caada6d3bde3106f5884718744e78fa9127ed0
Diggory Blake <diggsey@googlemail.com>
2020-12-23 17:59:26 +0000

    Numeric order never changes, so don't need a list for that

--- src/bin/day23b.rs
+++ src/bin/day23b.rs
@@ -1,66 +1,40 @@
 use std::mem;
 
 const INPUT: &str = "792845136";
-
-#[derive(Debug)]
-struct Cup {
-    value: u32,
-    circle_next: usize,
-    num_prev: usize,
-}
-
 const TOTAL_LEN: usize = 1000000;
 
+fn prev(n: usize) -> usize {
+    (n + TOTAL_LEN - 1) % TOTAL_LEN
+}
+
 fn main() {
-    let early_values: Vec<u32> = INPUT.chars().map(|c| c.to_digit(10).unwrap()).collect();
-    let one_pos = early_values.iter().copied().position(|v| v == 1).unwrap();
-
-    let mut values = Vec::new();
-    for value in early_values.iter().copied() {
-        let index = values.len();
-        values.push(Cup {
-            value,
-            circle_next: index + 1,
-            num_prev: early_values
-                .iter()
-                .copied()
-                .position(|v| v == value - 1)
-                .unwrap_or(TOTAL_LEN - 1),
-        });
+    let mut values: Vec<_> = (0..TOTAL_LEN).map(|v| v + 1).collect();
+    let mut current = 9;
+    for c in INPUT.chars().rev() {
+        let v = (c.to_digit(10).unwrap() - 1) as usize;
+        values[v] = current;
+        current = v;
     }
-    for value in 10..=TOTAL_LEN {
-        values.push(Cup {
-            value: value as u32,
-            circle_next: value % TOTAL_LEN,
-            num_prev: if value == 10 {
-                early_values.iter().copied().position(|v| v == 9).unwrap()
-            } else {
-                value - 2
-            },
-        })
-    }
-
-    let mut current = 0;
+    values[TOTAL_LEN - 1] = current;
     for _ in 0..10000000 {
-        let move1 = values[current].circle_next;
-        let move2 = values[move1].circle_next;
-        let move3 = values[move2].circle_next;
-        let move_end = values[move3].circle_next;
+        let move1 = values[current];
+        let move2 = values[move1];
+        let move3 = values[move2];
+        let move_end = values[move3];
 
-        let mut dst = values[current].num_prev;
+        let mut dst = prev(current);
         while dst == move1 || dst == move2 || dst == move3 {
-            dst = values[dst].num_prev;
+            dst = prev(dst);
         }
 
-        values[current].circle_next = move_end;
+        values[current] = move_end;
 
-        let dst_end = mem::replace(&mut values[dst].circle_next, move1);
-        values[move3].circle_next = dst_end;
-
+        let dst_end = mem::replace(&mut values[dst], move1);
+        values[move3] = dst_end;
         current = move_end;
     }
-    let a = values[one_pos].circle_next;
-    let b = values[a].circle_next;
-    let res = (values[a].value as u64) * (values[b].value as u64);
+    let a = values[0];
+    let b = values[a];
+    let res = ((a + 1) as u64) * ((b + 1) as u64);
     println!("{}", res);
 }
